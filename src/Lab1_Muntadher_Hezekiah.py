de80fb32657b8fc92658e4c71c99000720272e52
Muntadher Al-Bawi <muntadher.albawi@dcmail.ca>
2025-01-21 16:02:09 -0500

    New Project Created

---
+++ src/Lab1_Muntadher_Hezekiah.py
@@ -0,0 +1,9 @@
+# Program Name: Lab1_Muntadher_Hezekiah.py
+# Program Author: Muntadher Al-Bawi, Hezekiah Cua
+# Date: 21-01-25
+#
+# Description: Develop a Python program that generates secure passwords based on user-defined
+# criteria, such as length, number of letters, digits, and special characters.
+
+# Define alphabet array
+ENGLISH_ALPHABET = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
